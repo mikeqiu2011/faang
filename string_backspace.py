764f1c5777039825a3c0233c0b2b50396dcb3123
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-17 09:27:38 +0800

    recursive solution

--- string_backspace.py
+++ string_backspace.py
@@ -24,11 +24,21 @@ def get_string_after_backspace(string, result):
 
     get_string_after_backspace(string[:i+1], result)
 
+def compare_string(str1, str2):
+    result1 = []
+    result2 = []
+    get_string_after_backspace(str1, result1)
+    get_string_after_backspace(str2, result2)
+
+    print(result1)
+    print(result2)
+
+    return result1 == result2
 
 
+string1 = 'zx#ab#c'
+string2 = 'zag#c'
 
-string = 'zx#ab#c'
-result = []
 
-get_string_after_backspace(string, result)
+result = compare_string(string1, string2)
 print(result)
